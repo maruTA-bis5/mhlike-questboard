2b923ae593cb4212655d998d975f6478744425a5
bis5 <bis5@bis5.mydns.jp>
2013-08-23 08:00:36 +0900

    save password with hash

--- cakephp-2.3.9/app/Model/User.php
+++ cakephp-2.3.9/app/Model/User.php
@@ -1,6 +1,13 @@
 <?php
-
+App::uses('AuthComponent', 'Controller/Component');
 class User extends AppModel 
 {
 	public $useTable = 'user';
+
+	public function beforeSave($options = array())
+	{
+		if (isset($this->data[$this->alias]['password']))
+			$this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
+	}
+	
 }
